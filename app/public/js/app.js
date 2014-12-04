24914c1b75354c29d5d1bb5eadbae46fa3a6bbfb
jpbatz <jph@hawaii.edu>
2014-12-03 20:58:42 -1000

    MBA Level 1 requirements complete

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,11 +1,37 @@
 $(document).ready(function() {
-  
-  $.get( "/api/about.json", function( data ) {
-  // $.get( "/api/spec.json", function( data ) {
-    // $( "#stuff" ).html( data.title );
-    // $( "#stuff" ).html( data.date );
-    $( "#stuff" ).html( data.body );
-    alert( "Load was performed." );
-  }, "json");
+
+// runs when about is clicked
+$('.about').click(about_click);
+
+function about_click() {
+
+    $.get( "/api/about.json", function( data ) {
+      // $( "#stuff" ).html( data.title );
+      // $( "#stuff" ).html( data.date );
+      $( "#stuff" ).html( data.body );
+      // alert( "Load was performed." );
+    }, "json");
+
+  }
+
+// when page is loaded
+  about_click();
+
+
+
+// runs when specs is clicked
+$('.specs').click(spec_function);
+
+function spec_function() {
+
+    $.get( "/api/spec.json", function( data ) {
+      // $( "#stuff" ).html( data.title );
+      // $( "#stuff" ).html( data.date );
+      $( "#stuff" ).html( data.body );
+      // alert( "Load was performed." );
+    }, "json");
+
+  }
+
 
 });
